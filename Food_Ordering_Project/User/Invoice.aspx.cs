b74944c716ff706d1d7249cd1da4d9f56ef78508
Anamika Saha <155446516+anaTuli133@users.noreply.github.com>
2026-08-01 13:04:01 +0600

    Update Invoice.aspx.cs

--- Food_Ordering_Project/User/Invoice.aspx.cs
+++ Food_Ordering_Project/User/Invoice.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -40,29 +40,37 @@ namespace Food_Ordering_Project.User
             }
         }
 
-        protected void lbDownloadInvoice_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                string downloadPath = @"E:\order_invoice.pdf";
-                DataTable dtbl = GetOrderDetails();
-                ExportToPdf(dtbl, downloadPath, "Order Invoice");
+protected void lbDownloadInvoice_Click(object sender, EventArgs e)
+{
+    try
+    {
+        string folderPath = Server.MapPath("~/Invoices/");
 
-                WebClient client = new WebClient();
-                Byte[] buffer = client.DownloadData(downloadPath);
-                if (buffer != null)
-                {
-                    Response.ContentType = "application/pdf";
-                    Response.AddHeader("content-length", buffer.Length.ToString());
-                    Response.BinaryWrite(buffer);
-                }
-            }
-            catch (Exception ex)
-            {
-                lblMsg.Visible = true;
-                lblMsg.Text = "Error Message: -" + ex.Message.ToString();
-            }
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
         }
+
+        string fileName = "Order_Invoice_" + Request.QueryString["id"] + ".pdf";
+        string filePath = Path.Combine(folderPath, fileName);
+
+        DataTable dtbl = GetOrderDetails();
+
+        ExportToPdf(dtbl, filePath, "Order Invoice");
+
+        Response.Clear();
+        Response.ContentType = "application/pdf";
+        Response.AppendHeader("Content-Disposition", "attachment; filename=" + fileName);
+        Response.TransmitFile(filePath);
+        Response.Flush();
+        HttpContext.Current.ApplicationInstance.CompleteRequest();
+    }
+    catch (Exception ex)
+    {
+        lblMsg.Visible = true;
+        lblMsg.Text = "Error Message: " + ex.Message;
+    }
+}
 
         DataTable GetOrderDetails()
         {
@@ -90,67 +98,72 @@ namespace Food_Ordering_Project.User
             return dt;
         }
 
-        void ExportToPdf(DataTable dtblTable, String strPdfPath, string strHeader)
+void ExportToPdf(DataTable dtblTable, string strPdfPath, string strHeader)
+{
+    using (FileStream fs = new FileStream(strPdfPath, FileMode.Create, FileAccess.Write, FileShare.None))
+    {
+        Document document = new Document(PageSize.A4);
+
+        PdfWriter writer = PdfWriter.GetInstance(document, fs);
+
+        document.Open();
+
+        // Header
+        BaseFont bfntHead = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+        Font fntHead = new Font(bfntHead, 16, Font.BOLD, Color.GRAY);
+
+        Paragraph prgHeading = new Paragraph();
+        prgHeading.Alignment = Element.ALIGN_CENTER;
+        prgHeading.Add(new Chunk(strHeader.ToUpper(), fntHead));
+        document.Add(prgHeading);
+
+        // Author
+        BaseFont bfAuthor = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+        Font fntAuthor = new Font(bfAuthor, 8, Font.ITALIC, Color.GRAY);
+
+        Paragraph prgAuthor = new Paragraph();
+        prgAuthor.Alignment = Element.ALIGN_RIGHT;
+        prgAuthor.Add(new Chunk("Order From : Foodie Fast Food", fntAuthor));
+
+        if (dtblTable.Rows.Count > 0)
         {
-            FileStream fs = new FileStream(strPdfPath, FileMode.Create, FileAccess.Write, FileShare.None);
-            Document document = new Document();
-            document.SetPageSize(PageSize.A4);
-            PdfWriter writer = PdfWriter.GetInstance(document, fs);
-            document.Open();
-
-            //Report Header
-            BaseFont bfntHead = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-            Font fntHead = new Font(bfntHead, 16, 1, Color.GRAY);
-            Paragraph prgHeading = new Paragraph();
-            prgHeading.Alignment = Element.ALIGN_CENTER;
-            prgHeading.Add(new Chunk(strHeader.ToUpper(), fntHead));
-            document.Add(prgHeading);
-
-            //Author
-            Paragraph prgAuthor = new Paragraph();
-            BaseFont btnAuthor = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-            Font fntAuthor = new Font(btnAuthor, 8, 2, Color.GRAY);
-            prgAuthor.Alignment = Element.ALIGN_RIGHT;
-            prgAuthor.Add(new Chunk("Order From : Foodie Fast Food", fntAuthor));
             prgAuthor.Add(new Chunk("\nOrder Date : " + dtblTable.Rows[0]["OrderDate"].ToString(), fntAuthor));
-            document.Add(prgAuthor);
+        }
 
-            //Add a line seperation
-            Paragraph p = new Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, Color.BLACK, Element.ALIGN_LEFT, 1)));
-            document.Add(p);
+        document.Add(prgAuthor);
 
-            //Add line break
-            document.Add(new Chunk("\n", fntHead));
+        document.Add(new Paragraph(" "));
 
-            //Write the table
-            PdfPTable table = new PdfPTable(dtblTable.Columns.Count - 2);
-            //Table header
-            BaseFont btnColumnHeader = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-            Font fntColumnHeader = new Font(btnColumnHeader, 9, 1, Color.WHITE);
-            for (int i = 0; i < dtblTable.Columns.Count - 2; i++)
+        PdfPTable table = new PdfPTable(dtblTable.Columns.Count - 2);
+        table.WidthPercentage = 100;
+
+        Font headerFont = new Font(bfAuthor, 9, Font.BOLD, Color.WHITE);
+
+        for (int i = 0; i < dtblTable.Columns.Count - 2; i++)
+        {
+            PdfPCell cell = new PdfPCell();
+            cell.BackgroundColor = Color.GRAY;
+            cell.AddElement(new Chunk(dtblTable.Columns[i].ColumnName.ToUpper(), headerFont));
+            table.AddCell(cell);
+        }
+
+        Font dataFont = new Font(bfAuthor, 8, Font.NORMAL, Color.BLACK);
+
+        foreach (DataRow row in dtblTable.Rows)
+        {
+            for (int j = 0; j < dtblTable.Columns.Count - 2; j++)
             {
                 PdfPCell cell = new PdfPCell();
-                cell.BackgroundColor = Color.GRAY;
-                cell.AddElement(new Chunk(dtblTable.Columns[i].ColumnName.ToUpper(), fntColumnHeader));
+                cell.AddElement(new Chunk(row[j].ToString(), dataFont));
                 table.AddCell(cell);
             }
-            //table Data
-            Font fntColumnData = new Font(btnColumnHeader, 8, 1, Color.BLACK);
-            for (int i = 0; i < dtblTable.Rows.Count; i++)
-            {
-                for (int j = 0; j < dtblTable.Columns.Count - 2; j++)
-                {
-                    PdfPCell cell = new PdfPCell();
-                    //table.AddCell(dtblTable.Rows[i][j].ToString());
-                    cell.AddElement(new Chunk(dtblTable.Rows[i][j].ToString(), fntColumnData));
-                    table.AddCell(cell);
-                }
-            }
-
-            document.Add(table);
-            document.Close();
-            writer.Close();
-            fs.Close();
         }
+
+        document.Add(table);
+
+        document.Close();
+        writer.Close();
+    }
+}
     }
 }
